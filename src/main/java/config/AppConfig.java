4b6adb54d77c98f635ace7569b01ce0e29dcffa7
Latterfall <Latterfall@gmail.com>
2020-09-23 19:45:58 +0300

    Мелкие правки

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -1,9 +1,23 @@
 package config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
 @ComponentScan(basePackages = "services")
 public class AppConfig {
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames(
+                "static/index/lang/index",
+                "static/messages/lang/messages",
+                "static/login/lang/login",
+                "static/404/lang/404");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 }
